bfcf6e07dd2ecbfbc0206f324af87c642b4247a8
hejianglin <hejianglin@uino.com>
2023-09-17 11:43:18 +0800

    fix: 修改批量下载用户主页

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ class Scraper {
             'accept-encoding': 'gzip, deflate, br',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
             'referer': 'https://www.douyin.com/',
-            'cookie': "ttwid=1%7C0YBAnAwiC5T3U5yJi8RVXEK3DOwF_2vpJ7kVJJZe8HU%7C1666668932%7C21048e6555b73e8801d3956afc6130b4a05ae73a2eefe4d3fef5ef1b61caf0e9; __live_version__=%221.1.1.2586%22; odin_tt=a77b90afad5db31e86fe004b39c5f35423292023ce7837cde82fd1f7fe54278890ce24dc89e09c8a2e55b1f4904950a7b0fca6b4fbff3b549ba6d55a335373ec; pwa2=%223%7C0%7C0%7C0%22; s_v_web_id=verify_lkagpdq1_IuHpxJyS_q6YH_4AvH_8aNH_zhvGPr95Jrc8; passport_csrf_token=301cf539fb735ab77de7e382b0dd93e5; passport_csrf_token_default=301cf539fb735ab77de7e382b0dd93e5; bd_ticket_guard_client_data=eyJiZC10aWNrZXQtZ3VhcmQtdmVyc2lvbiI6MiwiYmQtdGlja2V0LWd1YXJkLWl0ZXJhdGlvbi12ZXJzaW9uIjoxLCJiZC10aWNrZXQtZ3VhcmQtcmVlLXB1YmxpYy1rZXkiOiJCRXhuWUdqREVBa3ErdjRsT2l3anRIWi9HU2hRNXFseWdJMklLanIxM0orRHozYnA0M2pXc3M3N25CUzdnbE5tTXhHbWU3cldoSE9pdkJvVmNnT2JiWFU9IiwiYmQtdGlja2V0LWd1YXJkLXdlYi12ZXJzaW9uIjoxfQ==; passport_assist_user=CkHJzB17Xsy3FUHyNfX2Dyb8IFKKA_0pu1SKYG0OAT_av3ImQyCbEmGJV7b8MJep4l9MjeCRK1FPY9k9yAkVHbIbvhpICjzS68aPlRjIsUzHLIEM-5jMbp9awcdJnkACni5Nnc_PBm4ljAlEqChbF4nYPpn4xyh4kY2hBvRikmXs0sgQ4fq2DRiJr9ZUIgEDbm8-yw%3D%3D; n_mh=13KNPUKNEzoW3A4J-OLRxfal2zj1GbF-vJUFPs3WSIY; sso_uid_tt=2581aab41d03156c0b7fee9c7e865c6c; sso_uid_tt_ss=2581aab41d03156c0b7fee9c7e865c6c; toutiao_sso_user=b2556b53ed5cee89e947b154b17645f1; toutiao_sso_user_ss=b2556b53ed5cee89e947b154b17645f1; sid_ucp_sso_v1=1.0.0-KDhlZjRhMmJhZGU0OTVmOWM0YzBkMTY5ZGNkZmI4NTFjNTk2ODU5OTkKHwiPluCxqYzbAhC29OKmBhjvMSAMMLDIpZkGOAZA9AcaAmhsIiBiMjU1NmI1M2VkNWNlZTg5ZTk0N2IxNTRiMTc2NDVmMQ; ssid_ucp_sso_v1=1.0.0-KDhlZjRhMmJhZGU0OTVmOWM0YzBkMTY5ZGNkZmI4NTFjNTk2ODU5OTkKHwiPluCxqYzbAhC29OKmBhjvMSAMMLDIpZkGOAZA9AcaAmhsIiBiMjU1NmI1M2VkNWNlZTg5ZTk0N2IxNTRiMTc2NDVmMQ; sid_guard=c1d1ac1d22198149dfc6cac74938b14a%7C1691925046%7C5184000%7CThu%2C+12-Oct-2023+11%3A10%3A46+GMT; uid_tt=7e39a426dac7802b2448fa2266ca1b85; uid_tt_ss=7e39a426dac7802b2448fa2266ca1b85; sid_tt=c1d1ac1d22198149dfc6cac74938b14a; sessionid=c1d1ac1d22198149dfc6cac74938b14a; sessionid_ss=c1d1ac1d22198149dfc6cac74938b14a; sid_ucp_v1=1.0.0-KDc4Y2VkZjIyN2JlMDNhYmNhYTFlYTE5ODM1YzI2YjVlZDNmMGY0N2YKGwiPluCxqYzbAhC29OKmBhjvMSAMOAZA9AdIBBoCbHEiIGMxZDFhYzFkMjIxOTgxNDlkZmM2Y2FjNzQ5MzhiMTRh; ssid_ucp_v1=1.0.0-KDc4Y2VkZjIyN2JlMDNhYmNhYTFlYTE5ODM1YzI2YjVlZDNmMGY0N2YKGwiPluCxqYzbAhC29OKmBhjvMSAMOAZA9AdIBBoCbHEiIGMxZDFhYzFkMjIxOTgxNDlkZmM2Y2FjNzQ5MzhiMTRh; LOGIN_STATUS=1; _bd_ticket_crypt_cookie=861cdca903469f36dd23fc1ecfe847c1; __security_server_data_status=1; store-region=us; store-region-src=uid; d_ticket=28acd5a9c6df4227b13582669694acded6ede; __ac_nonce=064ec4f3a00901157c769; __ac_signature=_02B4Z6wo00f01ve8HKgAAIDD6.-iFWbfM-r3jRgAANkQTCm7UjsJOQlMGY7o-iPsCIAe0kuriDaQ15lHcML.nW.cGNWpSBLUJzdr6s8KHRbqh5ywvupCeAKBEHKKbji7hD1-Z0x3DI-n0KKx34; douyin.com; device_web_cpu_core=16; device_web_memory_size=-1; webcast_local_quality=null; publish_badge_show_info=%220%2C0%2C0%2C1693208382348%22; IsDouyinActive=true; home_can_add_dy_2_desktop=%220%22; strategyABtestKey=%221693208382.387%22; stream_recommend_feed_params=%22%7B%5C%22cookie_enabled%5C%22%3Atrue%2C%5C%22screen_width%5C%22%3A1344%2C%5C%22screen_height%5C%22%3A756%2C%5C%22browser_online%5C%22%3Atrue%2C%5C%22cpu_core_num%5C%22%3A16%2C%5C%22device_memory%5C%22%3A0%2C%5C%22downlink%5C%22%3A%5C%22%5C%22%2C%5C%22effective_type%5C%22%3A%5C%22%5C%22%2C%5C%22round_trip_time%5C%22%3A0%7D%22; VIDEO_FILTER_MEMO_SELECT=%7B%22expireTime%22%3A1693813183367%2C%22type%22%3A1%7D; volume_info=%7B%22isUserMute%22%3Afalse%2C%22isMute%22%3Atrue%2C%22volume%22%3A1%7D; my_rd=1; passport_fe_beating_status=true; msToken=ESPx4FwNhcdEvr36-bmhWde9xupU_c64WeeqvvzqzLCtmEsvGPXhkwsKM8miaoC2w8gWSzNAfqxPEju4w3jzopIFompVSmwemq9-z1F8V-2vLNhTxLlYCUVdXkzNj6zM; download_guide=%221%2F20230828%2F0%22; csrf_session_id=3c194edf7f2cee968b0df65f97a11648; msToken=XFIGWeX20IGrrEUGYr_4SR2DPrduwK5zxB3gOp8FfbxW_Ng-w9uNh8wQRUIoPUtkSblL6msqte55jyfcrKPb8eDZekS9Q1P9hkdkPFiV4Ni-l9Vmsr0KgFo5MOkLaBZy; tt_scid=-i-7N5fAMRj8pGg4drGXbjasutdtD4tzIeqRnm6OJ1LoXRRZGl8FNhORnEuY3id.b3b7"
+            'cookie': 'n_mh=ZGkDUDq87_ReJpNDNjXJAcIlxSe3cA3dXF3pz7Vo1So; LOGIN_STATUS=1; store-region=cn-ah; store-region-src=uid; my_rd=1; ttwid=1%7CLkU2OLnKWs8DcVLGMP_dxHcLobk9Y_e-SSDlUKglsiM%7C1687349374%7C7ea67aa5225ad7fcef78191fd87dea42b2281cd2ffe1bcb205c5b9d5fe3f3ff7; s_v_web_id=verify_lkcquhow_M0SokhHW_aHUZ_4ui0_92nd_t45hIoyFwlF0; passport_csrf_token=ae1d4b9e0f6b3dd26dcdd858c74f1373; passport_csrf_token_default=ae1d4b9e0f6b3dd26dcdd858c74f1373; passport_assist_user=CjzG2846zH4nSFD12e3ghPYNBgVTysxLmt3P0hRO1e6x0z-quOC5aMrxB3mrdK60HTBt9XFvCfBsq5MfKqYaSAo8hG-cIbPHcQ1Aue3pau4dTSAcJ3AeIR8-NqaW0lrmyWdtRikQRByqm7IPca9yb-aF6r41GhlvdFAoD8gzEIq_uA0Yia_WVCIBA_D4Aq0%3D; sso_uid_tt=9630cb01a699aa5c33a8808e718ec11b; sso_uid_tt_ss=9630cb01a699aa5c33a8808e718ec11b; toutiao_sso_user=996cb6a83b1c5c2b713af855b64cc098; toutiao_sso_user_ss=996cb6a83b1c5c2b713af855b64cc098; odin_tt=5308c2d274686bdafa9204700857b3e8f169e15e0c9caadc1cf46bea997712ecb32029d28f08ad7147305fa071c5ba5a; uid_tt=d26841f0ed166bbf0e8fbba5a6eddd4e; uid_tt_ss=d26841f0ed166bbf0e8fbba5a6eddd4e; sid_tt=69b218330b62e948d2f62a8f1a8e698c; sessionid=69b218330b62e948d2f62a8f1a8e698c; sessionid_ss=69b218330b62e948d2f62a8f1a8e698c; _bd_ticket_crypt_cookie=9c7b4fc95ad98147cf18ff9498c897fc; __security_server_data_status=1; __live_version__=%221.1.1.3119%22; sid_ucp_sso_v1=1.0.0-KDcwNGVmYjFiMjJiMDkwYmY1Y2ZmOGVhYjc0ZWYyMWQ2Y2M2ZGM1NzAKHQj2h_PV7gEQqoTLpwYY7zEgDDCgos3LBTgGQPQHGgJobCIgOTk2Y2I2YTgzYjFjNWMyYjcxM2FmODU1YjY0Y2MwOTg; ssid_ucp_sso_v1=1.0.0-KDcwNGVmYjFiMjJiMDkwYmY1Y2ZmOGVhYjc0ZWYyMWQ2Y2M2ZGM1NzAKHQj2h_PV7gEQqoTLpwYY7zEgDDCgos3LBTgGQPQHGgJobCIgOTk2Y2I2YTgzYjFjNWMyYjcxM2FmODU1YjY0Y2MwOTg; sid_guard=69b218330b62e948d2f62a8f1a8e698c%7C1693631018%7C5184000%7CWed%2C+01-Nov-2023+05%3A03%3A38+GMT; sid_ucp_v1=1.0.0-KDg2ZDc3NDdmYzFlNGIyM2FkM2ZjN2RjMjQ1MjQ2MzQ3MmJhZWYwYmIKGQj2h_PV7gEQqoTLpwYY7zEgDDgGQPQHSAQaAmxxIiA2OWIyMTgzMzBiNjJlOTQ4ZDJmNjJhOGYxYThlNjk4Yw; ssid_ucp_v1=1.0.0-KDg2ZDc3NDdmYzFlNGIyM2FkM2ZjN2RjMjQ1MjQ2MzQ3MmJhZWYwYmIKGQj2h_PV7gEQqoTLpwYY7zEgDDgGQPQHSAQaAmxxIiA2OWIyMTgzMzBiNjJlOTQ4ZDJmNjJhOGYxYThlNjk4Yw; bd_ticket_guard_client_data=eyJiZC10aWNrZXQtZ3VhcmQtdmVyc2lvbiI6MiwiYmQtdGlja2V0LWd1YXJkLWl0ZXJhdGlvbi12ZXJzaW9uIjoxLCJiZC10aWNrZXQtZ3VhcmQtcmVlLXB1YmxpYy1rZXkiOiJCSi9YaFJNYkE1RlVGUzlldjFzcXpWZXNuRktqUmNxSjFnam9GQmdOMlJKclo3Wnc4N1JKTjZMdnZNV2lTLzFuWkorZFNqdUd5QWEvTHp3T2o4ZjhJbkk9IiwiYmQtdGlja2V0LWd1YXJkLXdlYi12ZXJzaW9uIjoxfQ==; __ac_nonce=065066d6d00dcec9d7f9d; __ac_signature=_02B4Z6wo00f01L2LOtwAAIDBQ8dG7f90QiS9qz5AAEppU8rmnlMQQsT0Wn6rUXj7BV7tk8Gud6ZIrtX.3ZI2WrJGBOfZkNeKvNOfm3PthvMBa4wmCicgabm09qyR4wvj-p4okPGfy5oYGH8R58; publish_badge_show_info=%220%2C0%2C0%2C1694920048606%22; home_can_add_dy_2_desktop=%220%22; strategyABtestKey=%221694920048.622%22; FOLLOW_LIVE_POINT_INFO=%22MS4wLjABAAAA1T1gJAQars4T_ve8V3T5Ld9J3GCAsNLpTlr6EhV36C0%2F1694966400000%2F0%2F1694920048827%2F0%22; volume_info=%7B%22isUserMute%22%3Afalse%2C%22isMute%22%3Afalse%2C%22volume%22%3A0.056%7D; csrf_session_id=6c84522dbdac6372ee9ae3ffbe850bcf; stream_recommend_feed_params=%22%7B%5C%22cookie_enabled%5C%22%3Atrue%2C%5C%22screen_width%5C%22%3A1920%2C%5C%22screen_height%5C%22%3A1080%2C%5C%22browser_online%5C%22%3Atrue%2C%5C%22cpu_core_num%5C%22%3A12%2C%5C%22device_memory%5C%22%3A8%2C%5C%22downlink%5C%22%3A10%2C%5C%22effective_type%5C%22%3A%5C%224g%5C%22%2C%5C%22round_trip_time%5C%22%3A100%7D%22; passport_fe_beating_status=true; msToken=4UD4cbYDcXUIVw66rmBqxwfRBKQfv0PaBaFZzMUt40-f92dR4decmUv4J334pfUdfukAVOEo5Kfmj-SjJh_gU8H03z1GZ1nDw8RWJRJp2wXSjlcNrBcoQw==; msToken=pG-e7TIA9uAyR8ui6-WI5IicyjjjOSkdNPwgIx_wqDzZxKz9ca3DPcZkzgiCByT4wcdWKKRrvt2MkRXB7q5auYl5wZZCxwBfhK8AfLebhBJm7HJaU1cKyw==; tt_scid=lb4xIawlluUIQ5M7sNX4dyoz.AvNwFZb9wJzYhW0Q6817bpVd84GEg3G.LUSKlgTa71b; download_guide=%222%2F20230917%2F0%22;'
             // 其他请求头
         };
     }
@@ -37,7 +37,11 @@ class Scraper {
             })
         })
     }
-
+    /**
+     * @description get sec_user_id by shared home page url
+     * @param {string} url 用户主页分享地址 
+     * @returns {string} sec_user_id
+     */
     getUserSecUidByShareUrl(url) {
         const headers = {
             authority: 'v.douyin.com',
@@ -135,81 +139,21 @@ class Scraper {
 
     /**
      * @description download video to local
+     * @param {string} videoId 视频的id
+     * @param {string} videoName 文件名称
+     * @param {string} dirname 目录地址
      */
-    async downloadVideo(videoId, desc, dirname) {
+    async downloadVideo(videoId, videoName, dirname) {
         const videoData = await this.getDouyinVideoData(videoId)
-        // await scraper.parserVideoData(douyinData)
         let url = await this.getDouyinNoWatermarkVideo(videoData);
-        await download(url, dirname ? `media/${dirname}` : 'media', { filename: `${desc}.mp4` })
+        await download(url, dirname ? `media/${dirname}` : 'media', { filename: `${videoName}.mp4` })
     }
 
-    async getAllAuthorVideoTest(sec_user_id = 'MS4wLjABAAAA3HyIgnzQJNSwe-rozfXm_cPf50bEPfouUlk6wlPQcQEgijzHyTzj6zAt6s0KRaXz', max_cursor = 0) {
-        let apiUrl = `https://www.douyin.com/aweme/v1/web/aweme/post/?aid=6383&channel=channel_pc_web&sec_user_id=${sec_user_id}&max_cursor={max_cursor}&webid=7259230874561693224`
-        const urlParser = new URL(apiUrl)
-        const query = urlParser.search.replace('?', '')
-        const xbogus = sign(query, this.headers['User-Agent'])
-        console.log('【parser】 生成的X-Bogus签名为: ' + xbogus)
-        const new_url = apiUrl + "&X-Bogus=" + xbogus
-        return new Promise((resolve, reject) => {
-            fetch(new_url, {
-                headers: this.douyinApiHeaders
-            }).then(res => console.log(res.body))
-                .then(json => {
-                    console.log(json)
-                    resolve(json)
-                })
-                .catch(err => reject(err));
-        })
-    }
-
-    async getAllVideos(result, maxCursor, sec_uid) {
-        let apiUrl = `https://www.iesdouyin.com/web/api/v2/aweme/post?sec_uid=${sec_uid}&count=20&max_cursor=${maxCursor}&aId=1128`;
-        const res = await fetch(apiUrl, { headers: this.douyinApiHeaders })
-        const data = await res.json()
-        if (data.aweme_list && data.aweme_list instanceof Array) {
-            const len = data.aweme_list.length
-            if (len === 0 || result > 20) {
-                return result
-            } else {
-                result = result.concat(data.aweme_list);
-                maxCursor = data.max_cursor;
-                result = await this.getAllVideos(result, maxCursor, sec_uid)
-            }
-        }
-    }
-
-    async getAuthorVideo(sec_uid) {
-        let result = [];
-        let maxCursor = 0;
-        console.log('sec_uid:' + sec_uid)
-        let apiUrl = `https://www.iesdouyin.com/web/api/v2/aweme/post?sec_uid=${sec_uid}&count=20&max_cursor=${maxCursor}&aId=1128`;
-        const res = await fetch(apiUrl, { headers: this.douyinApiHeaders })
-        const data = await res.json()
-        if (data.aweme_list && data.aweme_list instanceof Array) {
-            result = result.concat(data.aweme_list);
-        }
-        return result
-    }
     /**
-     * @description get author's all video
+     * @description batch download media to local
+     * @param {string} sec_user_id 用户id
      */
-    async getAllAuthorVideo(sec_uid) {
-        // TODO: fininsh page 
-        const result = await this.getAuthorVideo(sec_uid)
-        const videoIds = result.map(i => this.downloadVideo(i.aweme_id, i.desc, sec_uid))
-        Promise.allSettled(videoIds).then((results) => {
-            const isHasFailed = results.filter(res => res.status === 'rejected')
-            if (isHasFailed.length > 0) {
-                const errorMsg = isHasFailed.map(i => i.reason).join(",")
-                console.log('download failed:' + errorMsg)
-            } else {
-                console.log("all mission execed")
-            }
-        })
-    }
-
-
-    async test(sec_user_id = 'MS4wLjABAAAAx1JQivd74p1jMocy9aj3UQK4dj-FayClZT7lKqHXDdfw_3QDtuq0kHNH2lmJEHJQ') {
+    async batchDownload(sec_user_id) {
         let result = []
         let maxCursor = 0
         let awemeLen = 1;
@@ -220,19 +164,18 @@ class Scraper {
             const xbogus = sign(query, this.headers['User-Agent'])
             const new_url = apiUrl + "&X-Bogus=" + xbogus
             const headers = JSON.parse(JSON.stringify(this.douyinApiHeaders))
-            headers.cookie += 'sessionid=c6bb98799c00f7e99f00dafbecea0920;'
+            // headers.cookie += 'sessionid=69b218330b62e948d2f62a8f1a8e698c'
             const res = await fetch(new_url, { headers })
+            // console.log(new_url)
             const data = await res.json()
             const { aweme_list, max_cursor } = data
             if (max_cursor) maxCursor = max_cursor
             awemeLen = aweme_list.length
             result = result.concat(aweme_list)
-            // Delay for 3 seconds
-            await new Promise(resolve => setTimeout(resolve, 3000));
+            // 间隔一定随机时间防止被ban 
+            await new Promise(resolve => setTimeout(resolve, Math.random() * 1000));
         } while (awemeLen > 0)
-        // console.log('<><><>', result.map(i => i.desc))
         const authorName = getDeepProperty(result, '0.author.nickname')
-        // console.log(authorName)
         const videoIds = result.map(i => this.downloadVideo(i.aweme_id, i.desc, authorName))
         Promise.allSettled(videoIds).then((results) => {
             const isHasFailed = results.filter(res => res.status === 'rejected')
@@ -245,9 +188,11 @@ class Scraper {
         })
     }
 
-    async getTodayVideo (sec_user_id) {
-        
-    }
+    /**
+     * @description 获取今天的视频
+     * @param {string} sec_user_id 用户id
+     */
+    async getTodayVideo (sec_user_id) {}
 }
 
 module.exports = Scraper;
