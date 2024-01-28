450b886e8d0614bcd7c658642ec6ae457e70614e
jlhe15 <jlhe15>
2024-01-28 11:30:51 +0800

    fix: 修复sid_guard过期问题

--- test.js
+++ test.js
@@ -3,16 +3,15 @@ const Scraper = require('.')
 const scraper = new Scraper()
 
 // const douyinUrl = '7- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieApRo4r/'
-// const douyinUrl = '0.51 03/08 NjP:/ Y@z.tE 复制打开抖音，看看【颜值 可能有关】你愿意陪我去北京嘛ᜊ•ᴗ•ᜊ# 好困  https://v.douyin.com/ieYQPSW'
+const douyinUrl = '0.51 03/08 NjP:/ Y@z.tE 复制打开抖音，看看【颜值 可能有关】你愿意陪我去北京嘛ᜊ•ᴗ•ᜊ# 好困  https://v.douyin.com/ieYQPSW'
 
 const test = async (url) => { 
-    const sec_user_id = await scraper.getUserSecUidByShareUrl(url)
-    console.log('sec_user_id:' + sec_user_id)
-    const result = await scraper.batcgetHomeVideoshDownload(sec_user_id)
-    console.log(result)
+    const douyinId = await scraper.getDouyinVideoId(url);
+    const douyinData = await scraper.getDouyinVideoData(douyinId);
+    console.log(douyinData)
 }
 
-// test('7- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieApRo4r/')
+test(douyinUrl)
 // 1- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieAoHneS/
 // 7- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieAoE3ob/
-test('长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ie9A1PWy/')
+// test('长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ie9A1PWy/')
