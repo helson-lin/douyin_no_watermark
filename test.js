17b155ad8d17a7e9e641ca41e50cc634cc715b58
helsonlin <helsonlin@163.com>
2024-09-24 11:03:32 +0800

    ci: update pkg node version

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const Scraper = require('.')
 
 const scraper = new Scraper()
 
-const douyinUrl = '7- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieApRo4r/'
+const douyinUrl = '3.53 复制打开抖音，看看【cLean克泠的图文作品】假如你现在18岁 在挪威留学。# 挪威 # 旅行 ... https://v.douyin.com/ikpUN1Mg/ 03/24 A@g.Ox Njc:/ '
 // const douyinUrl = '0.51 03/08 NjP:/ Y@z.tE 复制打开抖音，看看【颜值 可能有关】你愿意陪我去北京嘛ᜊ•ᴗ•ᜊ# 好困  https://v.douyin.com/ieYQPSW'
 
 const test = async (url) => { 
@@ -12,6 +12,7 @@ const test = async (url) => {
 }
 
 test(douyinUrl)
+// 3.53 复制打开抖音，看看【cLean克泠的图文作品】假如你现在18岁 在挪威留学。# 挪威 # 旅行 ... https://v.douyin.com/ikpUN1Mg/ 03/24 A@g.Ox Njc:/ 
 // 1- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieAoHneS/
 // 7- 长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ieAoE3ob/
 // test('长按复制此条消息，打开抖音搜索，查看TA的更多作品。 https://v.douyin.com/ie9A1PWy/')
